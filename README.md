00d0dc0397398638ccbb4846d2b82c5525eec7ab
yuvakumar2110 <yuvakumar2110@gmail.com>
2024-07-12 20:32:29 +0530

    Changes

--- README.md
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-# Phising-Classifier
-
-Adding some changes
-=======
-# Phising-Classifier
->>>>>>> 209b4427b07766e3b33fe42f325614335a16fd71
--- data_ingestion/data_loader_prediction.py
+++ data_ingestion/data_loader_prediction.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 
 class Data_Getter_Pred:
@@ -39,45 +38,4 @@ class Data_Getter_Pred:
             raise Exception()
 
 
-=======
-import pandas as pd
 
-class Data_Getter_Pred:
-    """
-    This class shall  be used for obtaining the data from the source for prediction.
-
-    Written By: iNeuron Intelligence
-    Version: 1.0
-    Revisions: None
-
-    """
-    def __init__(self, file_object, logger_object):
-        self.prediction_file='Prediction_FileFromDB/InputFile.csv'
-        self.file_object=file_object
-        self.logger_object=logger_object
-
-    def get_data(self):
-        """
-        Method Name: get_data
-        Description: This method reads the data from source.
-        Output: A pandas DataFrame.
-        On Failure: Raise Exception
-
-         Written By: iNeuron Intelligence
-        Version: 1.0
-        Revisions: None
-
-        """
-        self.logger_object.log(self.file_object,'Entered the get_data method of the Data_Getter class')
-        try:
-            self.data= pd.read_csv(self.prediction_file) # reading the data file
-            self.logger_object.log(self.file_object,'Data Load Successful.Exited the get_data method of the Data_Getter class')
-            return self.data
-        except Exception as e:
-            self.logger_object.log(self.file_object,'Exception occured in get_data method of the Data_Getter class. Exception message: '+str(e))
-            self.logger_object.log(self.file_object,
-                                   'Data Load Unsuccessful.Exited the get_data method of the Data_Getter class')
-            raise Exception()
-
-
->>>>>>> 209b4427b07766e3b33fe42f325614335a16fd71
